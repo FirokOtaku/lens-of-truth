a84454b93bdba556cd5ff3f43bf4cb66ff95b25c
FirokOtaku <351768593@outlook.com>
2025-12-24 00:30:46 +0800

    0.7.0

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,68 @@ html, body, #app {
   width: 100%;
   height: 100%;
 }
+
+#app {
+background: linear-gradient(180deg,#536976,#292e49)
+}
+
 </style>
 <style scoped>
+
+.lot-title
+{
+  font-size: 4rem;
+  font-family: Blackadder ITC, system-ui;
+}
+.lot-input
+{
+  background: transparent;
+  backdrop-filter: blur(4px);
+}
 
 </style>
 
 <template>
-  <div class="full flex-vertical">
-    <div class="flex-fixed">
-      <n-flex size="large">
-        <n-page-header title="Lens of Truth" subtitle="微型自用服务">
-        </n-page-header>
+  <n-flex class="full" align="center" justify="center">
+    <div>
+      <n-flex justify="center">
+        <div class="lot-title">
+          Lens of Truth
+        </div>
+      </n-flex>
+
+      <n-flex :vertical="false">
+        <n-input class="lot-input"
+                 v-model="valueInputUrl"
+                 placeholder="输入链接" />
+
+        <n-flex class="full-width" justify="end">
+          <n-button type="info" ghost>
+            <n-icon size="1rem">
+              <Copy16Regular/>
+            </n-icon>
+            复制
+          </n-button>
+          <n-button type="primary" ghost>
+            <n-icon>
+
+            </n-icon>
+            导入
+          </n-button>
+        </n-flex>
       </n-flex>
     </div>
 
-    <div class="flex-dynamic">
-      <pre>{{ value }}</pre>
-    </div>
-
-    <div class="flex-fixed">
-      123
-    </div>
-
-  </div>
+  </n-flex>
 </template>
 
 <script setup lang="ts">
 
+import { Copy16Regular, } from '@vicons/fluent'
 import {ref, onMounted,} from 'vue'
 import axios from 'axios'
 
-const value = ref<any>(null)
+const valueInputUrl = ref<any>('')
 
 onMounted(async () => {
 
